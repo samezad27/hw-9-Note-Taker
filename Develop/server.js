ee6308cfe177ba21b055b6c9d28db8638282ad74
samuel ezadmanesh <samezad27@gmail.com>
2022-12-16 18:33:41 -0800

    connected server

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const path = require("path");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,6 +8,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
 
 //spin up server
 app.listen(PORT, () =>
